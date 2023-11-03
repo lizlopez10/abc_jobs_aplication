05a70cee5ed9e83dd3eaeb2aa47ee4831d3ff5f4
ErikBernal94 <ed.bernalc1@uniandes.edu.co>
2023-11-02 23:10:34 -0500

    Additional logic

--- scripts/ScriptDDL.sql
+++ scripts/ScriptDDL.sql
@@ -2,50 +2,18 @@
 
 CREATE SCHEMA evaluacion AUTHORIZATION postgres;
 
-
--- evaluacion.candidato definition
-
--- Drop table
-
--- DROP TABLE evaluacion.candidato;
-
-CREATE TABLE evaluacion.candidato (
-	id serial4 NOT NULL,
-	nombre varchar NULL,
-	apellido varchar NULL,
-	CONSTRAINT candidato_pk PRIMARY KEY (id)
-);
-
--- evaluacion.estadoevaluacion definition
+-- evaluacion.estado_evaluacion definition
 
 -- Drop table
 
--- DROP TABLE evaluacion.estadoevaluacion;
+-- DROP TABLE evaluacion.estado_evaluacion;
 
-CREATE TABLE evaluacion.estadoevaluacion (
+CREATE TABLE evaluacion.estado_evaluacion (
 	id serial4 NOT NULL,
 	descripcion varchar NULL,
 	CONSTRAINT estadoevaluacion_pk PRIMARY KEY (id)
-);-- evaluacion.evaluacion definition
-
--- Drop table
-
--- DROP TABLE evaluacion.evaluacion;
-
-CREATE TABLE evaluacion.evaluacion (
-	id serial4 NOT NULL,
-	idestadoevaluacion int4 NOT NULL,
-	idcandidato int4 NOT NULL,
-	calificacion numeric NULL,
-	ultimonivel int4 NULL DEFAULT 0,
-	CONSTRAINT evaluacion_pk PRIMARY KEY (id)
 );
 
-
--- evaluacion.evaluacion foreign keys
-
-ALTER TABLE evaluacion.evaluacion ADD CONSTRAINT evaluacion_fk FOREIGN KEY (idcandidato) REFERENCES evaluacion.candidato(id);
-ALTER TABLE evaluacion.evaluacion ADD CONSTRAINT evaluacion_fk_1 FOREIGN KEY (idestadoevaluacion) REFERENCES evaluacion.estadoevaluacion(id);
 
 -- evaluacion.pregunta definition
 
@@ -60,6 +28,27 @@ CREATE TABLE evaluacion.pregunta (
 	CONSTRAINT pregunta_pk PRIMARY KEY (id)
 );
 
+
+-- evaluacion.evaluacion definition
+
+-- Drop table
+
+-- DROP TABLE evaluacion.evaluacion;
+
+CREATE TABLE evaluacion.evaluacion (
+	id serial4 NOT NULL,
+	id_estado_evaluacion int4 NOT NULL,
+	id_candidato int4 NOT NULL,
+	calificacion numeric NULL,
+	ultimo_nivel int4 NULL DEFAULT 0,
+	fecha_inicio timestamp NULL,
+	tiempo_limite int4 NULL,
+	descripcion varchar NULL,
+	CONSTRAINT evaluacion_pk PRIMARY KEY (id),
+	CONSTRAINT estado_evaluacion_fk FOREIGN KEY (id_estado_evaluacion) REFERENCES evaluacion.estado_evaluacion(id)
+);
+
+
 -- evaluacion.respuesta definition
 
 -- Drop table
@@ -68,35 +57,27 @@ CREATE TABLE evaluacion.pregunta (
 
 CREATE TABLE evaluacion.respuesta (
 	id serial4 NOT NULL,
-	idpregunta int4 NOT NULL,
+	id_pregunta int4 NOT NULL,
 	respuesta varchar NOT NULL,
 	escorrecta bool NOT NULL,
-	CONSTRAINT respuest_pk PRIMARY KEY (id)
+	CONSTRAINT respuest_pk PRIMARY KEY (id),
+	CONSTRAINT respuesta_fk FOREIGN KEY (id_pregunta) REFERENCES evaluacion.pregunta(id)
 );
 
 
--- evaluacion.respuesta foreign keys
-
-ALTER TABLE evaluacion.respuesta ADD CONSTRAINT respuesta_fk FOREIGN KEY (idpregunta) REFERENCES evaluacion.pregunta(id);
-
-
--- evaluacion.preguntarespondida definition
+-- evaluacion.pregunta_respondida definition
 
 -- Drop table
 
--- DROP TABLE evaluacion.preguntarespondida;
+-- DROP TABLE evaluacion.pregunta_respondida;
 
-CREATE TABLE evaluacion.preguntarespondida (
+CREATE TABLE evaluacion.pregunta_respondida (
 	id serial4 NOT NULL,
-	idpregunta int4 NOT NULL,
-	idevaluacion int4 NOT NULL,
-	idrespuesta int4 NOT NULL,
-	CONSTRAINT preguntarespondida_pk PRIMARY KEY (id)
+	id_pregunta int4 NOT NULL,
+	id_evaluacion int4 NOT NULL,
+	id_respuesta int4 NOT NULL,
+	CONSTRAINT preguntarespondida_pk PRIMARY KEY (id),
+	CONSTRAINT preguntarespondida_fk FOREIGN KEY (id_pregunta) REFERENCES evaluacion.pregunta(id),
+	CONSTRAINT preguntarespondida_fk_1 FOREIGN KEY (id_evaluacion) REFERENCES evaluacion.evaluacion(id),
+	CONSTRAINT preguntarespondida_fk_2 FOREIGN KEY (id_respuesta) REFERENCES evaluacion.respuesta(id)
 );
-
-
--- evaluacion.preguntarespondida foreign keys
-
-ALTER TABLE evaluacion.preguntarespondida ADD CONSTRAINT preguntarespondida_fk FOREIGN KEY (idpregunta) REFERENCES evaluacion.pregunta(id);
-ALTER TABLE evaluacion.preguntarespondida ADD CONSTRAINT preguntarespondida_fk_1 FOREIGN KEY (idevaluacion) REFERENCES evaluacion.evaluacion(id);
-ALTER TABLE evaluacion.preguntarespondida ADD CONSTRAINT preguntarespondida_fk_2 FOREIGN KEY (idrespuesta) REFERENCES evaluacion.respuesta(id);
